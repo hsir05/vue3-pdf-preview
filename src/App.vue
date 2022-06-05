ec82da78618a705399ada4a66ced08ed5baf35f9
haqsir@163.com <haqsir@163.com>
2022-06-05 17:22:15 +0800

    vue3 pdf 预览功能实现

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+    <PDFView :pdfUrl="jsPdf" />
+</div>
+</template>
+<script setup lang="ts">
+import PDFView from "./components/pdfPreview.vue"
+import jsPdf from "./Javascript.pdf"
+</script>
