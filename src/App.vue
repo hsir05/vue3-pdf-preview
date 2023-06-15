d2c7251feb73b4e5c4d08dce4e2ef8457fad0977
haqsir@163.com <haqsir@163.com>
2023-06-15 13:59:19 +0800

    feat: pdf

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
-    <PDFView pdfUrl="http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf" />
+    <!-- <PDFView pdfUrl="http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf" /> -->
+    <PDFView2 />
 </div>
 </template>
 <script setup lang="ts">
-import PDFView from "./components/pdfPreview.vue"
+// import PDFView from "./components/pdfPreview.vue"
+import PDFView2 from "./components/pdfPreview2.vue"
+
 </script>
---
+++ src/components/pdfPreview2.vue
@@ -0,0 +1,14 @@
+<template>
+    <vue-pdf-app theme="dark" class="pdf" pdf="http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf"></vue-pdf-app>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import VuePdfApp from "vue3-pdf-app";
+import "vue3-pdf-app/dist/icons/main.css";
+    
+</script>
+<style >
+.pdf{
+    height: 300px;
+}
+</style>
